798b7b53ad88763fd6266bcdcae7830d26bf8dc7
StevenLomon <steven.cesario@proton.me>
2026-06-05 21:31:33 +0200

    create_user POST endpoint skeleton + json middleware

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -4,8 +4,24 @@ import dotenv from "dotenv";
 dotenv.config();
 let app = express();
 
+app.use(express.json());
+
 app.get("/", (req, res) => {
   res.send(":)");
+});
+
+app.post("/create-user", (req, res) => {
+  const joined_at = new Date().toLocaleString(); // I'll refine this later
+  // Will take username and raw_password from body
+  // TODO: If successful, add User to db
+  res.status(201).json({
+    "success": "ok",
+    "data": {
+      "username": req.body.username,
+      "joined_at": joined_at,
+      // Might add more
+    }
+  });
 });
 
 let port = process.env.PORT || 3000;
