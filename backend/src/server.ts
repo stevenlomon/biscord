1ed579e3ace5f74b1e41a829d1f0384c11e82693
StevenLomon <steven.cesario@proton.me>
2026-06-05 21:45:21 +0200

    express static + users.json + random uuid user_id

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -5,13 +5,16 @@ import bcrypt from "bcrypt";
 dotenv.config();
 let app = express();
 
+app.use('/static', express.static('public'));
 app.use(express.json());
 
 app.get("/", (req, res) => {
   res.send(":)");
 });
 
+// USER ENDPOINTS
 app.post("/create-user", async (req, res) => {
+  const user_id = crypto.randomUUID();
   const joined_at = new Date().toLocaleString(); // I'll refine this later
   const raw_password = req.body.password;
   const encryped_password = await bcrypt.hash(raw_password, 10); // I know from earlier Python that 10 is a reasonable standard for Salt Rounds. No AI needed! :D
@@ -21,6 +24,7 @@ app.post("/create-user", async (req, res) => {
   res.status(201).json({
     "success": "ok",
     "data": {
+      "id": user_id,
       "username": req.body.username,
       "encryped_pw": encryped_password,
       "joined_at": joined_at,
@@ -29,6 +33,13 @@ app.post("/create-user", async (req, res) => {
   });
 });
 
+// AUTH ENDPOINTS
+app.post("/login", (req, res) => {
+
+})
+
+
+// START WEB SERVER
 let port = process.env.PORT || 3000;
 
 app.listen(port, () => {
