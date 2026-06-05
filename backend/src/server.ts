592339b58a1ec45370b2ac02f5da428a40453ac6
StevenLomon <steven.cesario@proton.me>
2026-06-05 20:13:32 +0200

    bare bones server up and running

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,2 +1,15 @@
 import express from "express";
+import dotenv from "dotenv";
 
+dotenv.config();
+let app = express();
+
+app.get("/", (req, res) => {
+  res.send(":)");
+});
+
+let port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+  console.log(`Web server started and listening at port ${port}`);
+});
