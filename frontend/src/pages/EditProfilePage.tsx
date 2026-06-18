d3e246121a64775b0933480988dace4a971048fe
StevenLomon <steven.cesario@proton.me>
2026-06-18 13:05:18 +0200

    unsavedChanges optimization and simplification

--- frontend/src/pages/EditProfilePage.tsx
+++ frontend/src/pages/EditProfilePage.tsx
@@ -1,22 +1,25 @@
-import { useEffect, useState } from "react"
+import { useState } from "react"
 import { useAuth } from "../contexts/AuthContext"
 
 const EditProfilePage = () => {
   const { currentUser } = useAuth();
   const [displayName, setDisplayName] = useState(currentUser?.displayName);
   const [bio, setBio] = useState(currentUser?.bio);
-  const [unsavedChanges, setUnsavedChanges] = useState(false);
+  // const [unsavedChanges, setUnsavedChanges] = useState(false);
 
-  useEffect(() => {
-    // Guard Clause: If unsavedChanges is false, return early
-    if (!unsavedChanges) return
+  // useEffect(() => {
+  //   // Guard Clause: If unsavedChanges is false, return early
+  //   if (!unsavedChanges) return
 
-    // *If* we have unsaved changes.. and either display name or bio is changed *back* to the saved current User values, set unsavedChanges back to false
-    if (displayName === currentUser?.displayName && bio === currentUser?.bio) { // Should be AND! Not OR haha
-      setUnsavedChanges(false);
-    }
+  //   // *If* we have unsaved changes.. and either display name or bio is changed *back* to the saved current User values, set unsavedChanges back to false
+  //   if (displayName === currentUser?.displayName && bio === currentUser?.bio) { // Should be AND! Not OR haha
+  //     setUnsavedChanges(false);
+  //   }
 
-  }, [displayName, bio]); // Listens for changes in display name and bio
+  // }, [displayName, bio]); // Listens for changes in display name and bio
+
+  // Turns out... neither the state nor the useEffect is needed haha! We can calculate and check it immediately using derived state! This also removes the need for `setUnsavedChanges(true);` in the onChange handlers! Massive simplification 
+  const hasUnsavedChanges = displayName !== currentUser?.displayName || bio !== currentUser?.bio
 
   return (
     <div>
@@ -25,14 +28,9 @@ const EditProfilePage = () => {
           <label><strong>Display Name</strong></label>
           <input
             type="text"
-            value={displayName ? displayName : ""}
+            value={displayName ?? ""} // Nullish Coalescing Operator: If displayName exists, use it, else "". Functionally identical to `displayName ? displayName : ""` but shorter
             placeholder={currentUser?.username}
-            onChange={
-              (e) => {
-                setDisplayName(e.target.value)
-                setUnsavedChanges(true);
-              }
-            }
+            onChange={(e) => setDisplayName(e.target.value)}
           />
         </div>
 
@@ -40,20 +38,15 @@ const EditProfilePage = () => {
           <label><strong>Bio</strong></label>
           <p>You can use markdown and links if you'd like.</p>
           <textarea
-            value={bio ? bio : ""}
-            onChange={
-              (e) => {
-                setBio(e.target.value)
-                setUnsavedChanges(true)
-              }
-            }>
-          </textarea>
+            value={bio ?? ""}
+            onChange={(e) => setBio(e.target.value)}
+          />
           {/* TODO: 190 character remaining counter, emoji picker */}
         </div>
 
         {/* TODO: Careful — you have unsaved changes! (Reset) (Save Changes) */}
       </form>
-      {unsavedChanges ? "Careful — you have unsaved changes!" : "There are no unsaved changes"}
+      {hasUnsavedChanges ? "Careful — you have unsaved changes!" : "There are no unsaved changes"}
     </div>
   )
 }
